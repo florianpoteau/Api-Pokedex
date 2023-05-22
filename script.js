c4ffad1245a84ef6e4535da29a1e1d74b323352e
Poteau Florian <florianpoteau59@gmail.com>
2023-05-22 12:52:22 +0200

    ajout: fetch pour rechercher sans recharger

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+    
+    let liste = []
 
     fetch("https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0", "GET", recherche);
 
@@ -24,29 +26,38 @@ document.addEventListener('DOMContentLoaded', function () {
         //Permet d'envoyer la requêtes
         request.send()
     }
+    
 
     function recherche() {
         let result = JSON.parse(this.response);
 
-        for (let i = 0; i < result.results.length; i++) {
+        liste = result.results;
+
+        console.log(liste);
+
+        let joke = document.getElementById('jokes');
+    joke.innerHTML = '';
+
+        for (let i = 0; i < liste.length; i++) {
             //Je crée mon <li></li>
             let li = document.createElement('li');
             
-            res = result.results[i].name
+            res = liste[i].name;
             // Je récupère les nom avec la boucle
             li.innerHTML = res;
-            
 
-            let joke = document.getElementById('jokes').append(li);
+            joke.appendChild(li);
 
                 // Gestion du formulaire pour rechercher les pokémons
 
             let form = document.querySelector(".form")
 
-            // Event formulaire
+            // Evenénement formulaire
 
             form.addEventListener('submit', function(event) {
                 event.preventDefault(); // Empêche le rechargement de la page
+
+                
 
                 // On récupére la valeur de l'input
                 
@@ -55,33 +66,45 @@ document.addEventListener('DOMContentLoaded', function () {
                 const selectValue = document.getElementById("select").value
                 console.log(selectValue);
 
-                // filtrer avec le select par type de pokémon
-                
+                // Appel de la fonction select pour le filtre
+
                 fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`, "GET", select);
 
-                function select () {
-                    result = JSON.parse(this.response);
-                    console.log(result);
-                    if (selectValue == result.types[0].type.name || (result.types[1] && selectValue == result.types[1].type.name)) {
-                        li.innerHTML = `${result.name}`
-                        joke.append(li)
-                    }
-                    else {
-                        li.style.display = "none"
-                    }
-                }
-
-                
-
-                // Condition permettant de vérifier la saisie
+                // Appel de la fonction printY pour afficher les pokémons
 
                 if (searchValue == result.results[i].name){
-
                     
-            fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`, "GET", printY);
+                    fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`, "GET", printY);
+                    fetch("https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0", "GET", recherche);
+                }
+                
+                
+            function select () {
+            // filtrer avec le select par type de pokémon
 
+        const selectValue = document.getElementById("select").value
+        console.log(selectValue);
+            result = JSON.parse(this.response);
+            console.log(result);
+
+            // Condition permettant de vérifier la saisie
+                    
+            if (selectValue == result.types[0].type.name || (result.types[1] && selectValue == result.types[1].type.name)) {
+                li.innerHTML = `${result.name}`
+                li.style.listStyleType = "disc"
+                joke.append(li)
+            }
+
+            else {
+                li.innerHTML = ""
+                li.style.listStyleType = "none"
+            }
+        }
+
+        
 
         function printY() {
+
 
             // Afficher les donnée au format json
 
@@ -97,7 +120,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const image = document.querySelector(".image");
 
-            image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`
+            image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`;
+
 
             // Récupérer les statistiques du pokémon
 
@@ -124,7 +148,7 @@ document.addEventListener('DOMContentLoaded', function () {
             } else {
             type1.innerHTML = "Ce Pokémon n'a que un type";
             }
-          }}
+          }
 
             });
 
