85f2a34ae852033ca8e281b7518f292a576c158e
Poteau Florian <florianpoteau59@gmail.com>
2023-05-22 14:57:15 +0200

    ajout: affichage des images dans generation 1

--- script.js
+++ script.js
@@ -2,16 +2,51 @@ document.addEventListener('DOMContentLoaded', function () {
     
     let liste = []
 
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0", "GET", recherche);
 
     // condition permettant de récupérer d'autre pokémon dans la liste
 
-    const plus = document.querySelector(".plus")
+    // const plus = document.querySelector(".plus")
 
-    plus.addEventListener("click", () =>{
-        fetch("https://pokeapi.co/api/v2/pokemon/?limit=10&offset=10", "GET", recherche);
-        plus.style.display = "none"
+    // plus.addEventListener("click", () =>{
+    //     fetch("https://pokeapi.co/api/v2/pokemon/?limit=10&offset=10", "GET", recherche);
+    //     plus.style.display = "none"
+    // })
+
+    let gen = document.querySelector(".gen1");
+
+    gen.addEventListener("click", (e) =>{
+    e.preventDefault()
+        fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=10`, "GET", rechercheNom);
     })
+
+    function rechercheNom (){
+
+        let result = JSON.parse(this.response);
+
+        console.log(result.results.length);
+
+        for (i = 0; i<result.results.length; i++){
+
+            fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}/`, "GET", rechercheImage);
+            
+        }
+        }
+
+        function rechercheImage (){
+
+            const image = document.createElement("img")
+
+            console.log(image);
+            
+            image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png`
+
+            let imgSrc = document.querySelector(".imageSrc")
+
+            imgSrc.appendChild(image)
+
+        }
+
+    
 
 
 
@@ -28,131 +63,132 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     
 
-    function recherche() {
-        let result = JSON.parse(this.response);
+    // function recherche() {
+    //     let result = JSON.parse(this.response);
 
-        liste = result.results;
+    //     liste = result.results;
 
-        console.log(liste);
+    //     console.log(liste);
 
-        let joke = document.getElementById('jokes');
-    joke.innerHTML = '';
+    //     let joke = document.getElementById('jokes');
+    // joke.innerHTML = '';
 
-        for (let i = 0; i < liste.length; i++) {
-            //Je crée mon <li></li>
-            let li = document.createElement('li');
-            
-            res = liste[i].name;
-            // Je récupère les nom avec la boucle
-            li.innerHTML = res;
 
-            joke.appendChild(li);
+    //     for (let i = 0; i < liste.length; i++) {
+    //         //Je crée mon <li></li>
+    //         let li = document.createElement('li');
 
-                // Gestion du formulaire pour rechercher les pokémons
+    //         const image = document.querySelector(".image");
 
-            let form = document.querySelector(".form")
+    //         image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`;
 
-            // Evenénement formulaire
+    //         joke.appendChild(li);
 
-            form.addEventListener('submit', function(event) {
-                event.preventDefault(); // Empêche le rechargement de la page
+    //             // Gestion du formulaire pour rechercher les pokémons
+
+    //         let form = document.querySelector(".form")
+
+    //         // Evenénement formulaire
+
+    //         form.addEventListener('submit', function(event) {
+    //             event.preventDefault(); // Empêche le rechargement de la page
 
                 
 
-                // On récupére la valeur de l'input
+    //             // On récupére la valeur de l'input
                 
-                const searchValue = document.getElementById('poke').value;
+    //             const searchValue = document.getElementById('poke').value;
 
-                const selectValue = document.getElementById("select").value
-                console.log(selectValue);
+    //             // const selectValue = document.getElementById("select").value
+    //             // console.log(selectValue);
 
-                // Appel de la fonction select pour le filtre
+    //             // Appel de la fonction select pour le filtre
 
-                fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`, "GET", select);
+    //             // fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`, "GET", select);
 
-                // Appel de la fonction printY pour afficher les pokémons
+    //             // Appel de la fonction printY pour afficher les pokémons
 
-                if (searchValue == result.results[i].name){
+    //             if (searchValue == result.results[i].name){
                     
-                    fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`, "GET", printY);
-                    fetch("https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0", "GET", recherche);
-                }
+    //                 fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}`, "GET", printY);
+    //                 fetch("https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0", "GET", recherche);
+    //             }
                 
                 
-            function select () {
-            // filtrer avec le select par type de pokémon
+    //     //     function select () {
+    //     //     // filtrer avec le select par type de pokémon
 
-        const selectValue = document.getElementById("select").value
-        console.log(selectValue);
-            result = JSON.parse(this.response);
-            console.log(result);
+    //     // const selectValue = document.getElementById("select").value
+    //     // console.log(selectValue);
+    //     //     result = JSON.parse(this.response);
+    //     //     console.log(result);
 
-            // Condition permettant de vérifier la saisie
+    //     //     // Condition permettant de vérifier la saisie
                     
-            if (selectValue == result.types[0].type.name || (result.types[1] && selectValue == result.types[1].type.name)) {
-                li.innerHTML = `${result.name}`
-                li.style.listStyleType = "disc"
-                joke.append(li)
-            }
+    //     //     if (selectValue == result.types[0].type.name || (result.types[1] && selectValue == result.types[1].type.name)) {
+    //     //         li.innerHTML = `${result.name}`
+    //     //         li.style.listStyleType = "disc"
+    //     //         joke.append(li)
+    //     //     }
 
-            else {
-                li.innerHTML = ""
-                li.style.listStyleType = "none"
-            }
-        }
+    //     //     else {
+    //     //         li.innerHTML = ""
+    //     //         li.style.listStyleType = "none"
+    //     //     }
+    //     // }
 
         
 
-        function printY() {
+    //     // function printY() {
 
 
-            // Afficher les donnée au format json
+    //     //     // Afficher les donnée au format json
 
-            let pokemon = JSON.parse(this.response);
+    //     //     let pokemon = JSON.parse(this.response);
 
-            // Afficher en titre le nom du pokémon
+    //     //     // Afficher en titre le nom du pokémon
 
-            const h1 = document.querySelector(".h1")
+    //     //     const h1 = document.querySelector(".h2")
 
-            h1.innerHTML = searchValue
+    //     //     h1.innerHTML = searchValue
 
-            // Récupérer l'image du pokémon
+    //     //     // Récupérer l'image du pokémon
 
-            const image = document.querySelector(".image");
+    //     //     const image = document.querySelector(".image");
 
-            image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`;
+    //     //     image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`;
 
 
-            // Récupérer les statistiques du pokémon
+    //     //     // Récupérer les statistiques du pokémon
 
-            const tableau = [pokemon.stats[0].base_stat, pokemon.stats[1].base_stat, pokemon.stats[2].base_stat, pokemon.stats[3].base_stat, pokemon.stats[4].base_stat, pokemon.stats[5].base_stat]
+    //     //     const tableau = [pokemon.stats[0].base_stat, pokemon.stats[1].base_stat, pokemon.stats[2].base_stat, pokemon.stats[3].base_stat, pokemon.stats[4].base_stat, pokemon.stats[5].base_stat]
 
-            console.log(pokemon);
+    //     //     console.log(pokemon);
 
-            let stat = document.getElementById("stat");
+    //     //     let stat = document.getElementById("stat");
 
-            stat.innerHTML = `Vous avez sélectionnez le pokémon ${searchValue}, il a: <br> <ul> <li> ${tableau[0]} pv </li><br> <li> ${tableau[1]} attaque(s),</li> <br> <li> ${tableau[2]} defense(s),</li> <br> <li> ${tableau[3]} en attaque spéciale,</li><br> <li>${tableau[4]} en défense spéciale </li><br> </li><li>${tableau[5]} en vitesse</li> </ul>`
+    //     //     stat.innerHTML = `Vous avez sélectionnez le pokémon ${searchValue}, il a: <br> <ul> <li> ${tableau[0]} pv </li><br> <li> ${tableau[1]} attaque(s),</li> <br> <li> ${tableau[2]} defense(s),</li> <br> <li> ${tableau[3]} en attaque spéciale,</li><br> <li>${tableau[4]} en défense spéciale </li><br> </li><li>${tableau[5]} en vitesse</li> </ul>`
 
-            // Récupérer le type du pokémon
+    //     //     // Récupérer le type du pokémon
 
-            const type = document.getElementById("type")
+    //     //     const type = document.getElementById("type")
 
-            const type1 = document.getElementById("type1")
+    //     //     const type1 = document.getElementById("type1")
 
-            const tableauType = pokemon.types.map(type => type.type.name);
+    //     //     const tableauType = pokemon.types.map(type => type.type.name);
 
-            type.innerHTML = `Son premier type est ${tableauType[0]}`;
+    //     //     type.innerHTML = `Son premier type est ${tableauType[0]}`;
 
-            if (tableauType.length > 1) {
-            type1.innerHTML = `Son deuxième type est ${tableauType[1]}`;
-            } else {
-            type1.innerHTML = "Ce Pokémon n'a que un type";
-            }
-          }
+    //     //     if (tableauType.length > 1) {
+    //     //     type1.innerHTML = `Son deuxième type est ${tableauType[1]}`;
+    //     //     } else {
+    //     //     type1.innerHTML = "Ce Pokémon n'a que un type";
+    //     //     }
+    //     //   }
 
-            });
+    //         });
 
-    }
-    }
+    // }
+    // }
 }
 )
