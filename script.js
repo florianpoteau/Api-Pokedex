99020e6c942a36be08bf7f3d9d093bda1d236ad6
Poteau Florian <florianpoteau59@gmail.com>
2023-05-25 15:36:11 +0200

    modif: indentation

--- script.js
+++ script.js
@@ -1,179 +1,185 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    // Fetch
+// Fetch
 
-    function fetch(url, method, fun) {
-        //Initialisation de XHR
-        const request = new XMLHttpRequest();
-        request.addEventListener("load", fun);
-        //Spécifier le type d'appel [ GET, POST, PUT, PATCH, DELETE ] et l'URL
-        request.open(method, url);
-        //Spécification que je veux du JSON en type de retour
-        request.setRequestHeader('Accept', "application/json");
-        //Permet d'envoyer la requête
-        request.send();
-      }
+function fetch(url, method, fun) {
+    //Initialisation de XHR
+    const request = new XMLHttpRequest();
+    request.addEventListener("load", fun);
+    //Spécifier le type d'appel [ GET, POST, PUT, PATCH, DELETE ] et l'URL
+    request.open(method, url);
+    //Spécification que je veux du JSON en type de retour
+    request.setRequestHeader('Accept', "application/json");
+    //Permet d'envoyer la requête
+    request.send();
+  }
 
       
 
-      // création des boutons de génération 1 à 9
+  // création des boutons de génération 1 à 9
       
-      const flexGen = document.querySelector(".flexGen")
+  const flexGen = document.querySelector(".flexGen")
 
-      // On boucle les boutons de 1 à 9 pour les créer
+  // On boucle les boutons de 1 à 9 pour les créer
 
-      for (let i = 1; i <= 9; i++) {
-        const genButton = document.createElement("button");
-        genButton.classList.add("gen");
-        genButton.textContent = "Gen " + i;
+  for (let i = 1; i <= 9; i++) {
+    const genButton = document.createElement("button");
+    genButton.classList.add("gen");
+    genButton.textContent = "Gen " + i;
       
-        flexGen.appendChild(genButton);
+    flexGen.appendChild(genButton);
 
-        // Création d'un tableau de génération afin de le fournir en paramètre pour la fonction click_bouton_gen
-        const generations = ["1", "2","3","4","5","6","7", "8","9"]
+    // Création d'un tableau de génération afin de le fournir en paramètre pour la fonction click_bouton_gen
+    const generations = ["1", "2","3","4","5","6","7", "8","9"]
 
-        // Sélection de tous les boutons créer
-        let gen1 = document.querySelectorAll(".gen");
+    // Sélection de tous les boutons créer
+    let gen1 = document.querySelectorAll(".gen");
       
-        // Boucle permettant de récupérer et de afficher les générations de 1 à 9 sur chaque bouton
-      for (let i = 0; i<gen1.length; i++){
-      click_bouton_gen(gen1[i], generations[i]);
-      }
-      }
+    // Boucle permettant de récupérer et d'afficher les générations de 1 à 9 sur chaque bouton
+  for (let i = 0; i<gen1.length; i++){
+  click_bouton_gen(gen1[i], generations[i]);
+  }
+  }
 
-      // Fetch et fonction recherche permettant à l'input de rechercher un pokémon et d'afficher un pokémon avec ses statistiques
-      fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1000&offset=0`, "GET", recherche)
 
-      // Fonction click_bouton_gen appellé dans la boucle ligne 38
-      // evénement au click
-      function click_bouton_gen(gen, generation) {
-          gen.addEventListener("click", (e) => {
-            e.preventDefault();
 
-            // Fetch permettant de rechercher chaque pokémon de chaque génération avec la fonction rechercheImg
-            fetch(`https://pokeapi.co/api/v2/generation/${generation}`, "GET", rechercheImg());
-          });
-      }
+  // Fetch et fonction recherche permettant à l'input de rechercher un pokémon et d'afficher un pokémon avec ses statistiques
+  fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1000&offset=0`, "GET", recherche)
+
+  // Fonction click_bouton_gen
+  // evénement au click
+  function click_bouton_gen(gen, generation) {
+      gen.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        // Fetch permettant de rechercher chaque pokémon de chaque génération avec la fonction rechercheImg
+        fetch(`https://pokeapi.co/api/v2/generation/${generation}`, "GET", rechercheImg());
+      });
+  }
 
       
     
-      // Fonction permettant de récupérer les images et le texte dans les générations
-      function rechercheImg() {
-        return function () {
-          let result = JSON.parse(this.response);
-          console.log(result);
-          const titre = document.querySelector(".titreGeneration");
-          titre.style.color = "#f3bc57";
+  // Fonction permettant de récupérer les images et le texte dans les générations
+  function rechercheImg() {
+    return function () {
+      let result = JSON.parse(this.response);
+      console.log(result);
+      const titre = document.querySelector(".titreGeneration");
+      titre.style.color = "#f3bc57";
       
-          // Supprimez le contenu de l'élément imageSrc avant d'ajouter de nouvelles images
-          let imgSrc = document.querySelector(".imageSrc");
-          imgSrc.innerHTML = '';
+      // Supprimez le contenu de l'élément imageSrc avant d'ajouter de nouvelles images
+      let imgSrc = document.querySelector(".imageSrc");
+      imgSrc.innerHTML = '';
 
-          // Faire une boucle pour afficher 10 images par génération et parcourir la liste
-          for (let i = 0; i < result.pokemon_species.length; i++) {
-            const pokemon = result.pokemon_species[i];
-            console.log(result);
-            const pokemonIndex = pokemon.url.split("/").slice(-2, -1)[0];
+      // Faire une boucle pour afficher 10 images par génération et parcourir la liste
+      for (let i = 0; i < result.pokemon_species.length; i++) {
+        const pokemon = result.pokemon_species[i];
+        console.log(result);
+        titre.innerHTML = `Génération ${result.id}`
+        const pokemonIndex = pokemon.url.split("/").slice(-2, -1)[0];
       
-            const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
+        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
       
-            // Créez un élément img pour afficher les images des Pokémon
-            const imageElement = document.createElement("img");
-            imageElement.classList.add("test")
-            imageElement.src = imageUrl;
+        // Créez un élément img pour afficher les images des Pokémon
+        const imageElement = document.createElement("img");
+        imageElement.classList.add("test")
+        imageElement.src = imageUrl;
       
-            // Créez un élément p pour afficher le nom de chaque Pokémon
-            const nomPokemon = document.createElement("p");
-            nomPokemon.innerHTML = pokemon.name;
+        // Créez un élément p pour afficher le nom de chaque Pokémon
+        const nomPokemon = document.createElement("p");
+        nomPokemon.innerHTML = pokemon.name;
       
-            // Créez un conteneur d'image disposé en flex en utilisant classList.add et ajoutez les images et le texte à ce conteneur
-            const imageContainer = document.createElement("div");
-            imageContainer.classList.add("imageContainer");
-            imageContainer.appendChild(imageElement);
-            imageContainer.appendChild(nomPokemon);
+        // Créez un conteneur d'image disposé en flex en utilisant classList.add et ajoutez les images et le texte à ce conteneur
+        const imageContainer = document.createElement("div");
+        imageContainer.classList.add("imageContainer");
+        imageContainer.appendChild(imageElement);
+        imageContainer.appendChild(nomPokemon);
       
-            imgSrc.appendChild(imageContainer);
-          }
-        };
+        imgSrc.appendChild(imageContainer);
       }
+    };
+  }
 
-      // Function permettant d'afficher les statistiques d'un pokémon lors de l'envoie du formulaire avec le nom du pokémon
+  // Function permettant d'afficher les statistiques d'un pokémon lors de l'envoie du formulaire avec le nom du pokémon
 
-      function recherche (){
+  function recherche (){
 
-        let result = JSON.parse(this.response);
+    let result = JSON.parse(this.response);
 
-        const nomPoke = document.querySelector(".nomPoke")
-        const image = document.querySelector(".imagePokemon")
-        const form = document.querySelector(".form")
+    const nomPoke = document.querySelector(".nomPoke")
+    const image = document.querySelector(".imagePokemon")
+    const form = document.querySelector(".form")
 
-        form.addEventListener("submit", (e) =>{
-          e.preventDefault();
+    form.addEventListener("submit", (e) =>{
+      e.preventDefault();
 
-          const searchValue = document.querySelector("#poke").value.toLowerCase();
+      const searchValue = document.querySelector("#poke").value.toLowerCase();
 
-          for (let i = 0; i<result.results.length; i++){
+      for (let i = 0; i<result.results.length; i++){
 
-            if (result.results[i].name.includes(searchValue)){
-            fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1&offset=${i}`, "GET", rechercheNom)
-          }
+        if (result.results[i].name.includes(searchValue)){
+        fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1&offset=${i}`, "GET", rechercheNom)
+      }
         
-        function rechercheNom(){
+    function rechercheNom(){
 
-          let pokemon = JSON.parse(this.response);
+      let pokemon = JSON.parse(this.response);
 
-          nomPoke.innerHTML = result.results[i].name
+      nomPoke.innerHTML = result.results[i].name
 
-          image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`;
+      image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`;
 
-          fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.results[0].name}/`, "GET", rechercheStatistique)
-  
-        }
-
-        function rechercheStatistique (){
-          result = JSON.parse(this.response);
-
-          const hp = result.stats[0].base_stat
-    const attaque = result.stats[1].base_stat
-    const defense = result.stats[2].base_stat
-    const attaqueSp = result.stats[3].base_stat
-    const defenseSp = result.stats[4].base_stat
-    const speed = result.stats[5].base_stat
-          
-          console.log(result.stats[0].base_stat)
-
-          const ctx = document.getElementById('myChart');
-
-          new Chart(ctx, {
-            type: 'radar',
-            data: {
-              labels: ['hp', 'attaque', 'defense', 'attaque spéciale', 'défense spéciale', 'vitesse'],
-              datasets: [{
-                label: ['Statistique'],
-                data: [hp, attaque, defense, attaqueSp, defenseSp, speed],
-                borderWidth: 1
-              }]
-            },
-            options: {
-              scales: {
-                y: {
-                  beginAtZero: true
-                }
-              }
-            }
-          });
-
-          
-
-        }
-      
-      }
-        })
-        
-      }
-
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.results[0].name}/`, "GET", rechercheStatistique)
     }
-    )
+    }
+  })
+
+}
+
+
+
+    function rechercheStatistique (){
+      result = JSON.parse(this.response);
+
+      const hp = result.stats[0].base_stat
+const attaque = result.stats[1].base_stat
+const defense = result.stats[2].base_stat
+const attaqueSp = result.stats[3].base_stat
+const defenseSp = result.stats[4].base_stat
+const speed = result.stats[5].base_stat
+
+      const ctx = document.getElementById('myChart');
+
+      new Chart(ctx, {
+        type: 'pie',
+        data: {
+          labels: ['hp', 'attaque', 'defense', 'attaque spéciale', 'défense spéciale', 'vitesse'],
+          datasets: [{
+            label: ['Statistique'],
+            data: [hp, attaque, defense, attaqueSp, defenseSp, speed],
+            borderWidth: 1
+                
+          }]
+        },
+        options: {
+          plugins: {
+            customCanvasBackgroundColor: {
+              color: 'lightGreen',
+            }
+          },
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    
+      
+      
+   
+  }
+})
 
       
     
